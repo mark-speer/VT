d1bdaac013e70f4839b45936a51968fa333cc58d
Mark Speer <mark.speer11@gmail.com>
2021-11-05 14:57:48 -0400

    trying to add slick slider, commented out for now

---
+++ assets/js/slickSettings.js
@@ -0,0 +1,35 @@
+
+
+    $(document).ready(function(){
+        $('.trustedSlider').slick({
+          arrows: true,
+          autoplay: false,
+          
+        });
+      });
+
+      $('.trustedSlider').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
